94313ccc7102822374f13c6946cd7f53a6e9291f
MohamedAlyEtriby <mohamedalyetriby@gmail.com>
2023-03-19 21:29:06 +0200

    Updated Task

--- components/Slider1.js
+++ components/Slider1.js
@@ -1,10 +1,24 @@
 import React from "react";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const Slider1 = ({ data }) => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <div style={{ width: "100%" }}>
-      <img src={data} style={{ width: "100%", objectFit: "contain" }} />
-    </div>
+    <Slider {...settings} style={{width:"97.5%",margin:"auto"}}>
+      <div style={{ width: "100%" }}>
+        <img src={data} style={{ width: "100%", objectFit: "contain" }} />
+      </div>
+      <div style={{ width: "100%" }}>
+        <img src={data} style={{ width: "100%", objectFit: "contain" }} />
+      </div>
+    </Slider>
   );
 };
 
